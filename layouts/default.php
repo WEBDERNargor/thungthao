e277785819be6ec8d5f6e33e012477a0bf880cf5
WEBDERNargor <96941642+WEBDERNargor@users.noreply.github.com>
2025-11-18 21:36:43 +0700

    Replace Tailwind CSS CDN link with jsDelivr

--- layouts/default.php
+++ layouts/default.php
@@ -7,7 +7,7 @@ $config=getConfig();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tailwindcss-cdn@3.4.10/tailwindcss.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="/api/readfile/js.php?file=cookie"></script>
  
